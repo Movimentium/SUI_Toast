faeb0f4e44dbec97703db3888c77416e03feb6bd
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-10-22 18:07:30 +0200

    Initial Commit

---
+++ SUI_Toast/SUI_ToastApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUI_ToastApp.swift
+//  SUI_Toast
+//
+//  Created by Miguel Gallego on 22/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SUI_ToastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
